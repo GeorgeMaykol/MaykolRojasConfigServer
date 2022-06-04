36e7546eaf7ba27de336711172d3621613750ce4
Maykol Rojas <maykolroj@gmail.com>
2022-06-04 07:35:58 -0500

    Config Server Terminado

---
+++ src/test/java/com/idat/MaykolRojasConfigServer/MaykolRojasConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.MaykolRojasConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaykolRojasConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
